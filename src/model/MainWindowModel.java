4bbab691803b4fa3790346a482a98f5e881a9d82
zinuzian <zinuzian@naver.com>
2018-06-05 19:04:07 +0900

    Update MainWindowModel.java

--- src/model/MainWindowModel.java
+++ src/model/MainWindowModel.java
@@ -2,7 +2,7 @@ package model;
 
 public class MainWindowModel {
 
-	private static Boolean isLoaded = false;
+	private static Boolean isBothLoaded = false;
 	private static Boolean isCompared = false;
 	private static Boolean isHighlighted = false;
 	
@@ -12,20 +12,17 @@ public class MainWindowModel {
 	public static void setIsHighlighted(Boolean isHighlighted) {
 		MainWindowModel.isHighlighted = isHighlighted;
 	}
-	public static void setIsLoaded(Boolean isLoaded) {
-		MainWindowModel.isLoaded = isLoaded;
+	public static void setIsBothLoaded(Boolean isLoaded) {
+		MainWindowModel.isBothLoaded = isLoaded;
 	}
 	public static void setIsCompared(Boolean isCompared) {
 		MainWindowModel.isCompared = isCompared;
 	}
-	public static Boolean getIsLoaded() {
-		return isLoaded;
+	public static Boolean getIsBothLoaded() {
+		return isBothLoaded;
 	}
 	public static Boolean getIsCompared() {
 		return isCompared;
-	}
-	public static void loaded() {
-		isLoaded = true;
 	}
 	public static void compared() {
 		isCompared = true;
@@ -33,4 +30,5 @@ public class MainWindowModel {
 	public static void highlighted() {
 		isHighlighted = true;
 	}
+	
 }
