c05b3f6761ba763ab0b86f43a6fdf348ca22d064
zinuzian <zinuzian@naver.com>
2018-06-05 21:06:03 +0900

    Revert "up down button added."
    This reverts commit 7f2fc72186705e959f010ea8c80c6f08be22a70b.

--- src/controller/MainWindowController.java
+++ src/controller/MainWindowController.java
@@ -57,8 +57,6 @@ public class MainWindowController {
 		this.view.getMergePanel().addCmpActionListener(new CmpActionListener());
 		this.view.getMergePanel().addCopyToLeftActionListener(new CopyToLeftActionListener());
 		this.view.getMergePanel().addCopyToRightActionListener(new CopyToRightActionListener());
-		this.view.getMergePanel().addUpActionListener(new UpActionListener());
-		this.view.getMergePanel().addDownActionListener(new DownActionListener());
 	}
 
 	class CmpActionListener implements ActionListener {
@@ -156,23 +154,5 @@ public class MainWindowController {
 			}
 		}
 
-	}
-	class UpActionListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	class DownActionListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
 	}
 }
